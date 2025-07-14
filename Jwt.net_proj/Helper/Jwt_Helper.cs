5a09baf14877e9ed4de9655815224fceb1ff3ea2
Mansoor Ahmad <ssgkhan653@gmail.com>
2025-07-14 10:35:14 +0500

    Changes in Jwt_Helper.

---
+++ Jwt.net_proj/Helper/Jwt_Helper.cs
@@ -0,0 +1,51 @@
+﻿using system Security.Claims;
+using Jwt.net_proj.Models;
+
+
+using System;
+
+namespace Jwt.net_proj.Entities
+
+
+{
+    public static class Jwt_Helper
+{
+	public static Jwt_Helper()
+	{
+			private static JwtSetting _jwtSetting;
+		public static	void Initialize(JwtSetting jwtSetting)
+		{
+			_jwtSetting = jwtSetting ?? throw new ArgumentNullException(nameof(jwtSetting),
+
+	    }
+
+	public static string GenerateAccessToken( IEnumerable<claim> claims)
+		{
+			var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_jwtSetting.SecretKey));
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+			var token = new JwtSecurityToken(
+				issuer: _jwtSetting.Issuer,
+				audience: _jwtSetting.Audience,
+				claims: claims,
+				expires: DateTime.Now.AddMinutes(_jwtSetting.ExpirationMinutes),
+				signingCredentials: creds
+				);
+			return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+	public static string GenerateRefreshToken()
+		{
+			var randomNumber = new byte[32];
+			using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+			{
+				rng.GetBytes(randomNumber);
+				var refreshToken = Convert.ToBase64String(randomNumber);
+			
+            }
+                
+		}
+																													
+    }
+
+}
+	
---
+++ Jwt.net_proj/Models/Jwt_Setting.cs
@@ -0,0 +1,17 @@
+﻿public class Jwt_Setting
+{
+    public string SecretKey { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int ExpirationMinutes { get; set; } = 60;
+    public int RefreshTokenExpirationDays { get; set; } = 30;
+    public Jwt_Setting() { }
+    public Jwt_Setting(string secretKey, string issuer, string audience, int expirationMinutes, int refreshTokenExpirationDays)
+    {
+        SecretKey = secretKey;
+        Issuer = issuer;
+        Audience = audience;
+        ExpirationMinutes = expirationMinutes;
+        RefreshTokenExpirationDays = refreshTokenExpirationDays;    
+    }
+}
