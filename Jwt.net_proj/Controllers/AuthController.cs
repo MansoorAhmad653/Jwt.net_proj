6303b5f77851869c083a19dd40b679c25d51c151
Mansoor Ahmad <ssgkhan653@gmail.com>
2025-07-14 11:29:28 +0500

    Error
    Got some error while running the proj  using the  program.cs

---
+++ Jwt.net_proj/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using YourProject.Helpers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        // Normally: validate user credentials first
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, "123"),
+            new Claim(ClaimTypes.Name, request.Username),
+            new Claim(ClaimTypes.Role, "User")
+        };
+
+        var accessToken = JwtHelper.GenerateAccessToken(claims);
+        var refreshToken = JwtHelper.GenerateRefreshToken();
+
+        return Ok(new
+        {
+            AccessToken = accessToken,
+            RefreshToken = refreshToken
+        });
+    }
+}
+    
--- Jwt.net_proj/Program.cs
+++ Jwt.net_proj/Program.cs
@@ -1,14 +1,43 @@
 using Scalar.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Jwt.net_proj.Models;
+using Microsoft.Extensions.Options;
+using Jwt.net_proj.Jwt_ Helper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<Jwt_Setting>(builder.Configuration.GetSection("JwtSettings"));
+var jwtSettings = app.Services.GetRequiredService<IOptions<Jwt_Setting>>().Value;
+
+// Add authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+        };
+    });
+
+builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi("v2");
 
 var app = builder.Build();
+
+// get and pass setting to helper
+Jwt_Helper.Initialize(jwtSettings);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,8 +48,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
